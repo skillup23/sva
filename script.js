76951cf840b19234327be6c061a0fc93c413831d
Robert Anisimov <skillup23@yandex.ru>
2024-08-10 12:52:58 +0300

    Версия 0.2

--- script.js
+++ script.js
@@ -1,47 +1,71 @@
-const urlLink = "https://jsonplaceholder.typicode.com/posts";
-const urlLink2 = "https://jsonplaceholder.typicode.com/albums";
+const apiLink = "https://jsonplaceholder.typicode.com/posts";
+const apiLink2 = "https://jsonplaceholder.typicode.com/albums";
 
 const tableContainer = document.querySelector(".table__container");
 const buttonUpdate = document.querySelector(".header__btn");
 
-async function getResponse(url) {
-  let response = await fetch(url);
-  let content = await response.json();
+async function getResponse(api) {
+  try {
+    const response = await fetch(api);
+    if (response.ok) {
+      const data = await response.json();
 
-  tableContainer.innerHTML = "";
+      createTable(data);
+    } else {
+      console.log("Error HTTP: " + response.status);
+    }
+  } catch (error) {
+    console.log("Ошибка в запросе:" + error.message);
+  }
+}
 
-  content.reverse().forEach((element) => {
-    createTable(element);
+// function getResponse(api) {
+//   fetch(api)
+//     .then((res) => {
+//       if (res.ok) {
+//         return res.json();
+//       } else {
+//         console.log("Error HTTP: " + response.status);
+//       }
+//     })
+//     .then((data) => {
+//       createTable(data);
+//     })
+//     .catch((error) => {
+//       console.log("Ошибка в запросе:" + error.message);
+//     });
+// }
+
+getResponse(apiLink);
+
+function createTable(array) {
+  array.reverse().forEach((item) => {
+    const tableTemplate = document.querySelector("#table-template").content;
+    const tableElement = tableTemplate.querySelector(".table").cloneNode(true);
+
+    if (!item.id) {
+      tableElement.querySelector(".id__array").textContent = "нет данных";
+    } else {
+      tableElement.querySelector(".id__array").textContent = item.id;
+    }
+
+    if (!item.title) {
+      tableElement.querySelector(".title__array").textContent = "нет данных";
+    } else {
+      tableElement.querySelector(".title__array").textContent = item.title;
+    }
+
+    if (!item.body) {
+      tableElement.querySelector(".body__array").textContent = "нет данных";
+    } else {
+      tableElement.querySelector(".body__array").textContent = item.body;
+    }
+
+    tableContainer.prepend(tableElement);
   });
 }
 
-getResponse(urlLink);
-
-function createTable(item) {
-  const tableTemplate = document.querySelector("#table-template").content;
-  const tableElement = tableTemplate.querySelector(".table").cloneNode(true);
-
-  if (!item.id) {
-    tableElement.querySelector(".id__array").textContent = "нет данных";
-  } else {
-    tableElement.querySelector(".id__array").textContent = item.id;
-  }
-
-  if (!item.title) {
-    tableElement.querySelector(".title__array").textContent = "нет данных";
-  } else {
-    tableElement.querySelector(".title__array").textContent = item.title;
-  }
-
-  if (!item.body) {
-    tableElement.querySelector(".body__array").textContent = "нет данных";
-  } else {
-    tableElement.querySelector(".body__array").textContent = item.body;
-  }
-
-  tableContainer.prepend(tableElement);
-}
-
 buttonUpdate.addEventListener("click", function () {
-  getResponse(urlLink2);
+  tableContainer.innerHTML = "";
+  getResponse(apiLink2);
 });
