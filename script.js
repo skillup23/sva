4cedbba42193e5637cefaf18d4de105c6a963c06
Robert Anisimov <skillup23@yandex.ru>
2024-09-26 16:45:30 +0300

    Верисия 0.3

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-const apiLink = "https://jsonplaceholder.typicode.com/posts";
-const apiLink2 = "https://jsonplaceholder.typicode.com/albums";
+const apiLink = 'https://jsonplaceholder.typicode.com/posts';
+const apiLink2 = 'https://jsonplaceholder.typicode.com/albums';
+const apiLink3 = 'https://sva.corp.rarus-cloud.ru/UNF/hs/api_sva/price/get';
+const apiLink4 = 'https://skillup23-backend-timeweb-b537.twc1.net/sva';
+const apiLink5 = 'http://localhost:3456/sva';
 
-const tableContainer = document.querySelector(".table__container");
-const buttonUpdate = document.querySelector(".header__btn");
+const tableContainer = document.querySelector('.table__container');
+const buttonUpdate = document.querySelector('.header__btn');
 
 async function getResponse(api) {
   try {
     const response = await fetch(api);
+
     if (response.ok) {
       const data = await response.json();
 
       createTable(data);
     } else {
-      console.log("Error HTTP: " + response.status);
+      console.log('Error HTTP: ' + response.status);
     }
   } catch (error) {
-    console.log("Ошибка в запросе:" + error.message);
+    console.log('Ошибка в запросе:' + error.message);
   }
 }
 
@@ -36,36 +40,124 @@ async function getResponse(api) {
 //     });
 // }
 
-getResponse(apiLink);
+// function getResponse(api) {
+//   fetch(api, {
+//     headers: new Headers({
+//       Authorization: token,
+//     }),
+//     // mode: 'no-cors',
+//   })
+//     .then((response) => {
+//       if (!response.ok) throw new Error(response.status);
+//       return response.json();
+//     })
+//     .catch((error) => {
+//       console.log('Ошибка в запросе:' + error.message);
+//     });
+// }
+
+getResponse(apiLink4);
 
 function createTable(array) {
-  array.reverse().forEach((item) => {
-    const tableTemplate = document.querySelector("#table-template").content;
-    const tableElement = tableTemplate.querySelector(".table").cloneNode(true);
+  const table = document.querySelector('.table__container');
 
-    if (!item.id) {
-      tableElement.querySelector(".id__array").textContent = "нет данных";
+  let list = '';
+
+  array.forEach((item) => {
+    if (!item.Картинка) {
+      foto = '/placeholder.jpg';
     } else {
-      tableElement.querySelector(".id__array").textContent = item.id;
+      foto = `data:image/jpeg;base64,${item.Картинка}`;
     }
 
-    if (!item.title) {
-      tableElement.querySelector(".title__array").textContent = "нет данных";
-    } else {
-      tableElement.querySelector(".title__array").textContent = item.title;
-    }
+    list += `<div class="table">
+          <div class="table__section table__img">
+            <img
+              src="${foto}"
+              alt="Фотография"
+              class="card__foto"
+              id="base64imagenone"
+            />
+          </div>
 
-    if (!item.body) {
-      tableElement.querySelector(".body__array").textContent = "нет данных";
-    } else {
-      tableElement.querySelector(".body__array").textContent = item.body;
-    }
-
-    tableContainer.prepend(tableElement);
+          <div class="table__section">
+            <div class="table__item">
+              <h4 class="nomenclature">${item.Номенклатура}</h4>
+            </div>
+            <div class="table__item table__item_bottom">
+              <p>Характеристика</p>
+              <h4 class="characteristic">${item.Характеристика}</h4>
+            </div>
+            <div class="table__item table__item_bottom">
+              <p>Остаток</p>
+              <h4 class="total">${item.Остаток} ${item.Единица}</h4>
+            </div>
+            <div class="table__item table__item_bottom">
+              <p>Цена</p>
+              <h4 class="price">${item.Цена} руб.</h4>
+            </div>
+          </div>
+        </div>`;
   });
+
+  // const tableTemplate = document.querySelector('#table-template').content;
+  // const tableElement = tableTemplate.querySelector('.table').cloneNode(true);
+
+  // console.log(item);
+
+  // if (!item.Номенклатура) {
+  //   tableElement.querySelector('.nomenclature').textContent = 'нет данных';
+  // } else {
+  //   tableElement.querySelector('.nomenclature').textContent =
+  //     item.Номенклатура;
+  // }
+
+  // if (!item.Характеристика) {
+  //   tableElement.querySelector('.characteristic').textContent = 'нет данных';
+  // } else {
+  //   tableElement.querySelector('.characteristic').textContent =
+  //     item.Характеристика;
+  // }
+
+  // if (!item.Остаток) {
+  //   tableElement.querySelector('.total').textContent = 'нет данных';
+  // } else {
+  //   tableElement.querySelector('.total').textContent = item.Остаток;
+  // }
+
+  // if (!item.Цена) {
+  //   tableElement.querySelector('.price').textContent = 'нет данных';
+  // } else {
+  //   tableElement.querySelector('.price').textContent = item.Цена;
+  // }
+
+  // if (!item.Картинка) {
+  //   tableElement.querySelector('.card__foto').src = '/placeholder.png';
+  // } else {
+  //   // const kartinka = `data:image/jpeg;base64,${String(item.Картинка)}`;
+
+  //   // const reg = /\n+/g;
+
+  //   // let imageBase64 = kartinka.replace(reg, '');
+
+  //   // console.log(kartinka.replace(reg, ''));
+
+  //   // console.log(String(kartinka));
+  //   console.log(item.Картинка);
+
+  //   const kartinka = 111
+
+  //   tableElement.querySelector(
+  //     'card__foto'
+  //   ).src = `data:image/jpeg;base64,${kartinka}`;
+  // }
+
+  // tableContainer.prepend(tableElement);
+
+  table.innerHTML = list;
 }
 
-buttonUpdate.addEventListener("click", function () {
-  tableContainer.innerHTML = "";
-  getResponse(apiLink2);
+buttonUpdate.addEventListener('click', function () {
+  tableContainer.innerHTML = '';
+  getResponse(apiLink5);
 });
